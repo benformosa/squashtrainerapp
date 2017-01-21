6b39ebecf12063f13a82dbb2769d0adf7089fe8f
Ben Formosa <ben.formosa@gmail.com>
2017-01-21 16:24:32 +1100

    Rough out UI for routine page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,26 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  items: any[];
 
   constructor(public navCtrl: NavController) {
+    this.items = [];
+    this.items.push({
+        id: 1,
+        name: "Forehand drives",
+        thumbnail: "squash.png",
+        shortDescription: "Hit above line, bounce before service box.",
+        description: "Practice hitting consistent length by targeting the cut line having the ball bounce between the service box and back wall. Try to avoid hitting the side wall.",
+        time: "00:01:00"
+    });
+    this.items.push({
+        id: 2,
+        name: "Forehand defense",
+        thumbnail: "squash.png",
+        shortDescription: "Target outline, lifting ball. Bounce before back wall",
+        description: "Practice lifting the ball to get out of trouble by targeting the outline and having the ball bounce before the back wall. Try to avoid hitting the side wall.",
+        time: "00:03:33"
+    });
     
   }
 
