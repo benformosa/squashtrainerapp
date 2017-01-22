025e599189772f242207657f8c3315d57057a82b
Ben Formosa <ben.formosa@gmail.com>
2017-01-22 19:23:27 +1100

    Split classes to separate files

---
+++ src/app/classes/activities.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ActivitiesService {
+    private activityUrl = '../../assets/activities.json';
+    constructor(private http: Http) { }
+
+    getActivities() {
+        return this.http.get(this.activityUrl)
+            .map(res => res.json());
+    }
+}
---
+++ src/app/classes/activity.class.ts
@@ -0,0 +1,6 @@
+export class Activity {
+  id: string;
+  name: string;
+  shortDescription: string;
+  description: string;
+}
