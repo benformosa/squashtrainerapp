1da5b03313e7795dcf1273e6791cafc2de949215
Ben Formosa <ben.formosa@gmail.com>
2017-01-24 20:02:19 +1100

    Split out "routineOutline" concept from "routine"

--- src/app/services/routineoutline.service.ts
+++ src/app/services/routineoutline.service.ts
@@ -1,16 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
-import { Activity } from '../classes/activity.class';
-import { Routine } from '../classes/routine.class';
-import { RoutineItem } from '../classes/routineitem.class';
 
 @Injectable()
-export class RoutineService {
+export class RoutineOutlineService {
     private routineUrl: string;
     constructor(private http: Http) { }
 
-    getRoutine(routineUrl: string, activities: Activity[]) {
+    getRoutineOutline(routineUrl: string) {
         this.routineUrl = routineUrl;
         return this.http.get(this.routineUrl)
             .map(res => res.json());
